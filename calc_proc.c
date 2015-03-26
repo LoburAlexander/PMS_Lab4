cabc02bf5b7ab232829e5b599f877b22b67ec7df
alex <alex@alex-pc.(none)>
2015-03-26 11:08:06 +0400

    Added second operand

--- calc_proc.c
+++ calc_proc.c
@@ -12,7 +12,7 @@ ssize_t calc_result_read(struct file *filp, char *buf, size_t count, loff_t *off
 	int len;
 	char number[15];
 	
-	len = sprintf(number, "%d\n", first);
+	len = sprintf(number, "%d\n", first + second);
 
 	if (count < len)
 		return -EINVAL;
@@ -27,12 +27,7 @@ ssize_t calc_result_read(struct file *filp, char *buf, size_t count, loff_t *off
 	return len;
 }
 
-static const struct file_operations calc_result_fops = {
-	.owner = THIS_MODULE,
-	.read = calc_result_read,
-};
-
-ssize_t calc_int_write(struct file *filp,const char *buf,size_t count,loff_t *offp)
+ssize_t calc_first_write(struct file *filp,const char *buf,size_t count,loff_t *offp)
 {
 	int i = 0;
 	first = 0;
@@ -41,9 +36,29 @@ ssize_t calc_int_write(struct file *filp,const char *buf,size_t count,loff_t *of
 	return count;
 }
 
+ssize_t calc_second_write(struct file *filp,const char *buf,size_t count,loff_t *offp)
+{
+	int i = 0;
+	second = 0;
+	for (i = 0; i < count - 1; ++ i)
+		second = second * 10 + (buf[i]-'0');
+	return count;
+}
+
+
+static const struct file_operations calc_result_fops = {
+	.owner = THIS_MODULE,
+	.read = calc_result_read,
+};
+
 static const struct file_operations calc_first_fops = {
 	.owner = THIS_MODULE,
-	.write = calc_int_write,
+	.write = calc_first_write,
+};
+
+static const struct file_operations calc_second_fops = {
+	.owner = THIS_MODULE,
+	.write = calc_second_write,
 };
 
 
@@ -51,6 +66,7 @@ static int __init calc_proc_init(void)
 {
 	proc_create("calc_proc_result", 0, NULL, &calc_result_fops);
 	proc_create("calc_proc_first", 0666, NULL, &calc_first_fops);
+	proc_create("calc_proc_second", 0666, NULL, &calc_second_fops);
 	return 0;
 }
 
@@ -58,6 +74,7 @@ static void __exit calc_proc_exit(void)
 {
 	remove_proc_entry("calc_proc_result", NULL);
 	remove_proc_entry("calc_proc_first", NULL);
+	remove_proc_entry("calc_proc_second", NULL);
 }
 
 MODULE_LICENSE("GPL");
--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-make
+make >> log.txt
 sudo insmod ./calc_proc.ko
-echo 1234 > /proc/calc_proc_first
+echo 12 > /proc/calc_proc_first
+echo 32 > /proc/calc_proc_second
 cat /proc/calc_proc_result
 sudo rmmod calc_proc
-make clean
+make clean >> log.txt
